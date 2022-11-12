8b525bcd84ca5af7d68365b476bf04a54b674008
ValentinLanglet59 <valentin.langlet59@gmail.com>
2022-11-12 09:19:38 +0100

    feat: added routes, articles controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'talk', to: 'pages#talk'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 end
