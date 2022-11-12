56e53fd4ff8968ce799076367d3dab3d3f255ace
ValentinLanglet59 <valentin.langlet59@gmail.com>
2022-11-12 16:48:12 +0100

    feat : destroy method done, crud functionning

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,4 +36,11 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    end
+  end
 end
